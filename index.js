c9aa6fb2bdd98789bc6cba429052b757cf7ecc1f
jinnosukeKato <jinnosukeKato@gmail.com>
2024-01-17 15:34:06 +0900

    add date to clock

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ function livelyCurrentTrack(data) {
   }
 }
 
-function clock() {
+function clock_update() {
+  const time_elm = document.getElementById("clock_time");
+  const date_elm = document.getElementById("clock_date");
+
   const data = new Date();
-  const clock_elm = document.getElementById("clock_data");
-  clock_elm.textContent = `${data.getHours().toString().padStart(2, "0")}:${data.getMinutes().toString().padStart(2, "0")}:${data.getSeconds().toString().padStart(2, "0")}`;
+  time_elm.textContent = `${data.getHours().toString().padStart(2, "0")}:${data.getMinutes().toString().padStart(2, "0")}:${data.getSeconds().toString().padStart(2, "0")}`;
+  date_elm.textContent = `${(data.getMonth() + 1).toString().padStart(2, "0")} / ${data.getDate().toString().padStart(2, "0")}`
 }
 
 function main() {
-  clock();
+  clock_update();
 }
 
 setInterval(main, 500);
